d0f167de77a63a69167218dd9394ccd1a8d01800
Sergiu Botnaruc <botnaruc@yahoo.com>
2022-02-22 19:40:49 +0200

    telegram bot

--- bot.py
+++ bot.py
@@ -1,199 +1,85 @@
-from telegram.ext import Updater, CommandHandler, RegexHandler, MessageHandler,Filters
-from telegram import ReplyKeyboardMarkup,Bot
+from telegram.ext import updater, MessageHandler, Updater, CommandHandler
+from telegram.ext.filters import Filters
 import requests,json
 import os
+import redis
+from datetime import date
+
+redcon = redis.StrictRedis(host='localhost', port=6379, db=2)
 
 config = json.load(open('config.json','r'))
-
 TOKEN = config['token']
-DEV = True
-signup = config['signup']
-refr = config['ref']
-admins = config['admins']
-data = []
-dash_key = [['Twitter','Discord','ETH address'],['Referral Link','Referred'],['Balance','Details']]
-admin_key = [['Users','Get List']]
+print(TOKEN)
 
-webhook_url = 'Your Webook'
+DEV = False
+updater = Updater(TOKEN, use_context=True)
+dp = updater.dispatcher
+
+
+
+stabila_group = config['stabilagroup']
+
+webhook_url = 'https://stabilascan.org/api/utils/telegram-webhooks/'
 PORT = int(os.environ.get('PORT','8443'))
 
+def new_user(update):
+    new_members = update.message.new_chat_members
+    print(new_members)
+    # do your stuff here:
+    for member in new_members:
+        print(member)
+        #print('1',member['username'])
+        redcon.hset(member['username'],'id',member['id'])
+        redcon.hset(member['username'],'isbot', "False")
+        redcon.hset(member['username'],'joined', "True")
+        times = redcon.hget(member['username'],'jtimes')
+        if times!=None:
+            times=int(times)+1
+            redcon.hset(member['username'], 'jtimes', times)
+        else:
+            redcon.hset(member['username'],'jtimes', 1)
+
+
+    update.message.reply_text("Welcome "+member['username'])
+
+def left_user(update):
+    left_member = update.message.left_chat_member
+    # do your stuff here:
+    print(left_member)
+        #print('1',member['username'])
+
+    redcon.hset(left_member['username'],'joined', "False")
+    times = redcon.hget(left_member['username'], 'ltimes')
+    if times != None:
+        times = int(times) + 1
+        redcon.hset(left_member['username'], 'ltimes', times)
+    else:
+        redcon.hset(left_member['username'], 'ltimes', 1)
+    update.message.reply_text("Bye "+left_member['username'])
 
 def start(update, context):
     if update.message.chat.type == 'private':
         user = str(update.message.chat.username)
-        if user not in data['users']:
-            data['users'].append(user)
-            if user not in data['twitter']:
-                data['twitter'][user] = ""
-            if user not in data['eth']:
-                data['eth'][user] = ""
-            if user not in data['discord']:
-                data['discord'][user] = ""
-            ref_id = update.message.text.split()
-            if len(ref_id) > 1:
-                data['ref'][user] = ref_id[1]
-                if str(ref_id[1]) not in data['referred']:
-                    data['referred'][str(ref_id[1])] = 1
-                else:
-                    data['referred'][str(ref_id[1])] += 1
-            else:
-                data['ref'][user] = 0
-            data['total'] += 1
-            data['id'][user] = data['total']
-            data['process'][user] = "twitter"
-            json.dump(data,open('users.json','w'))
-            msg = config['intro']
-            started_msg = 'TWITTER MESSAGE'
-            update.message.reply_text(msg)
-            update.message.reply_text(started_msg)
-        else:
-            welcome_msg = "DASHBOARD MESSAGE"
-            reply_markup = ReplyKeyboardMarkup(dash_key,resize_keyboard=True)
-            update.message.reply_text(welcome_msg,reply_markup=reply_markup)
+        print(user)
 
-    else:
-        msg = '{} \n. I don\'t reply in group, come in private'.format(config['intro'])
-        update.message.reply_text(msg)
+def main():
 
-def twitter(update, context):
-    if update.message.chat.type == 'private':
-        user = str(update.message.chat.username)
-        twtr_user = data['twitter'][user]
-        msg = 'Your twitter username is {}'.format(twtr_user)
-        reply_markup = ReplyKeyboardMarkup(dash_key,resize_keyboard=True)
-        update.message.reply_text(msg,reply_markup=reply_markup)
+    print("1")
+    dp.add_handler(MessageHandler(Filters.status_update.new_chat_members, new_user))
+    print("2")
+    dp.add_handler(MessageHandler(Filters.status_update.left_chat_member, left_user))
+    dp.add_handler(MessageHandler(Filters.regex("start"), start))
 
-def eth(update, context):
-    if update.message.chat.type == 'private':
-        user = str(update.message.chat.username)
-        eth_addr = data['eth'][user]
-        msg = 'Your eth address is {}'.format(eth_addr)
-        reply_markup = ReplyKeyboardMarkup(dash_key,resize_keyboard=True)
-        update.message.reply_text(msg,reply_markup=reply_markup)
-
-def discord(update, context):
-    if update.message.chat.type == 'private':
-        user = str(update.message.chat.username)
-        du = data['discord'][user]
-        msg = 'Your Discord username is {}'.format(du)
-        reply_markup = ReplyKeyboardMarkup(dash_key,resize_keyboard=True)
-        update.message.reply_text(msg,reply_markup=reply_markup)
-
-def link(update, context):
-    if update.message.chat.type == 'private':
-        user = str(update.message.chat.username)
-        msg = 'https://t.me/{}?start={}'.format(config['botname'],data['id'][user])
-        reply_markup = ReplyKeyboardMarkup(dash_key,resize_keyboard=True)
-        update.message.reply_text(msg,reply_markup=reply_markup)
-
-
-def extra(update, context):
-    if update.message.chat.type == 'private':
-        user = str(update.message.chat.username)
-        if data["process"][user] == 'twitter':
-            data['twitter'][user] = update.message.text
-            data['process'][user] = 'discord'
-            json.dump(data,open('users.json','w'))
-            update.message.reply_text("DISCORD MESSAGE")
-        elif data["process"][user] == 'discord':
-            data['discord'][user] = update.message.text
-            data['process'][user] = "eth"
-            json.dump(data,open('users.json','w'))
-            update.message.reply_text("WALLET MESSAGE")
-        elif data["process"][user] == 'eth':
-            data['eth'][user] = update.message.text
-            data['process'][user] = "finished"
-            json.dump(data,open('users.json','w'))
-            msg = "DASHBOARD MESSAGE!"
-            reply_markup = ReplyKeyboardMarkup(dash_key,resize_keyboard=True)
-            update.message.reply_text(msg,reply_markup=reply_markup)
-        else:
-            msg = "Please select one of the options."
-            reply_markup = ReplyKeyboardMarkup(dash_key,resize_keyboard=True)
-            update.message.reply_text(msg,reply_markup=reply_markup)
-
-def ref(update, context):
-    if update.message.chat.type == 'private':
-        user = str(update.message.chat.username)
-        i = str(data["id"][user])
-        referred = 0
-        if i in data['referred']:
-            referred = data['referred'][i]
-        msg = "You have referred {} people".format(referred)
-        reply_markup = ReplyKeyboardMarkup(dash_key,resize_keyboard=True)
-        update.message.reply_text(msg,reply_markup=reply_markup)
-
-def admin(update, context):
-    if update.message.chat.type == 'private':
-        user = str(update.message.chat.username)
-        if user in admins:
-            msg = "Welcome to Admin Dashboard"
-            reply_markup = ReplyKeyboardMarkup(admin_key,resize_keyboard=True)
-            update.message.reply_text(msg,reply_markup=reply_markup)
-
-def users(update, context):
-    if update.message.chat.type == 'private':
-        user = str(update.message.chat.username)
-        if user in admins:
-            msg = "A total of {} have joined this program".format(data['total']-1000)
-            reply_markup = ReplyKeyboardMarkup(admin_key,resize_keyboard=True)
-            update.message.reply_text(msg,reply_markup=reply_markup)
-
-def get_file(update, context):
-    if update.message.chat.type == 'private':
-        user = str(update.message.chat.username)
-        if user in admins:
-            f = open('users.csv','w')
-            f.write("id,username,twitter username,eth address,discord,no. of persons referred,referred by\n")
-            for u in data['users']:
-                i = str(data['id'][u])
-                refrrd = 0
-                if i in data['referred']:
-                    refrrd = data['referred'][i]
-                d = "{},{},{},{},{},{},{}\n".format(i,u,data['twitter'][u],data['eth'][u],data['discord'][u],refrrd,data['ref'][u])
-                f.write(d)
-            f.close()
-            bot = Bot(TOKEN)
-            bot.send_document(chat_id=update.message.chat.id, document=open('users.csv','rb'))
-
-def bal(update, context):
-    if update.message.chat.type == 'private':
-        user = str(update.message.chat.username)
-        i = str(data["id"][user])
-        referred = 0
-        if i in data['referred']:
-            referred = data['referred'][i]
-        bal = signup + refr * referred
-        msg = "You have {} tokens".format(bal)
-        reply_markup = ReplyKeyboardMarkup(dash_key,resize_keyboard=True)
-        update.message.reply_text(msg,reply_markup=reply_markup)
-
-def detail(update, context):
-    if update.message.chat.type == 'private':
-        msg = config['details']
-        reply_markup = ReplyKeyboardMarkup(dash_key,resize_keyboard=True)
-        update.message.reply_text(msg,reply_markup=reply_markup)
 
 if __name__ == '__main__':
-    data = json.load(open('users.json','r'))
-    updater = Updater(TOKEN,use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start",start))
-    dp.add_handler(CommandHandler("admin",admin))
-    dp.add_handler(RegexHandler("^Twitter$",twitter))
-    dp.add_handler(RegexHandler("^ETH address$",eth))
-    dp.add_handler(RegexHandler("^Discord$",discord))
-    dp.add_handler(RegexHandler("^Referral Link$",link))
-    dp.add_handler(RegexHandler("^Referred$",ref))
-    dp.add_handler(RegexHandler("^Users$",users))
-    dp.add_handler(RegexHandler("^Get List$",get_file))
-    dp.add_handler(RegexHandler("^Balance$",bal))
-    dp.add_handler(RegexHandler("^Details$",detail))
-    dp.add_handler(MessageHandler(Filters.text,extra))
+
+    main()
     if DEV is not True:
+        print(webhook_url+TOKEN)
         updater.start_webhook(listen="0.0.0.0",port=PORT,url_path=TOKEN)
         updater.bot.set_webhook(webhook_url + TOKEN)
     else:
-        updater.start_polling()
+        print("4")
+        updater.start_polling(clean=True)
     print("Bot Started")
     updater.idle()
